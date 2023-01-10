47c42ca087f129049efcfd8289c1a785b1a16454
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-10 16:26:46 -0300

    Git fim do dia

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   darkMode: "class",
   theme: {
     fontFamily: {
-      display: ["Open Sans", "sans-serif"],
-      body: ["Open Sans", "sans-serif"],
+      display: ["Montserrat", "sans-serif"],
+      body: ["Montserrat", "sans-serif"],
     },
     extend: {
       fontSize: {
