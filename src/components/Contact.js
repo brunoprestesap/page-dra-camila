604455da85484a84909379a1bdf837ca9baebb5e
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-17 19:18:32 -0300

    alteracao na secao de contato

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
                 </div>
             </div>
 
-            <div className="md:flex justify-center gap-5">
+            <div className="md:flex text-left gap-5">
                 <iframe
                     className="w-full rounded-xl"
                     title="Onde eu atendo"
@@ -21,21 +21,21 @@ function Contact() {
                     allowFullScreen
                     src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ7YYbzwvhYY0RY0V-Dkp_Am8&key=AIzaSyAKyXjIGENqlWQFa4Pzk5HwUhuxpnmLZ_s">
                 </iframe>
-                <div className="flex flex-col content-center text-center px-12 gap-2 mt-5">
-                    <div className="flex flex-col items-center">
+                <div className="flex flex-col text-left px-12 gap-2 mt-5">
+                    <div className="flex gap-3 items-center text-left">
                         <FaMapMarkerAlt className="text-4xl mb-3"/>
-                        <h2 className="font-bold text-xl">Meu endereço</h2>
+                        <h2 className="font-bold text-2xl">Meu endereço</h2>
                     </div>
 
-                    <div className="flex justify-center items-center">
+                    <div className="flex text-left">
                         
                         <h3>Rua Manoel Eudóxio Pereira, 826 - Centro. CEP 68900-021. Macapá / Amapá.</h3>
                     </div>
 
-                    <h2 className="font-bold text-xl">Contato</h2>
+                    <h2 className="font-bold text-2xl">Contato</h2>
 
-                    <div className="flex justify-center items-center gap-5">
-                        <FaMobileAlt className="text-2xl" />
+                    <div className="flex items-center gap-3">
+                        <FaMobileAlt className="text-4xl" />
                         <a href="https://wa.me/5596981163765" target="_blank" rel="noreferrer">
                             <h3 className="text-xl">(96)98116-3765</h3>
                         </a>
