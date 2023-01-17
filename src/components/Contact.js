afbc1fe83b2745b9c9ba40ef3a56d62ece4960e9
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-17 07:52:27 -0300

    alterações no contato

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { FaHeadset, FaHospitalAlt } from "react-icons/fa"
+import { FaHospitalAlt, FaMobileAlt } from "react-icons/fa"
 
 function Contact() {
     return (
@@ -11,30 +11,32 @@ function Contact() {
             </div>
 
             <div className="md:flex justify-center gap-5">
-                <iframe 
+                <iframe
                     className="w-full rounded-xl"
                     title="Onde eu atendo"
-                    width="600" 
+                    width="600"
                     height="500"
-                    loading="lazy" 
-                    allowFullScreen 
+                    loading="lazy"
+                    allowFullScreen
                     src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ7YYbzwvhYY0RY0V-Dkp_Am8&key=AIzaSyAKyXjIGENqlWQFa4Pzk5HwUhuxpnmLZ_s">
                 </iframe>
                 <div className="flex flex-col content-center text-center px-12 gap-2 mt-5">
-                    <h2 className="font-bold text-xl">Meu local de atendimento</h2>
-                    
-                    <div className="flex items-center gap-2">
-                        <FaHospitalAlt className="text-6xl"/>
+                    <h2 className="font-bold text-xl">Meu endereço</h2>
+
+                    <div className="flex justify-center items-center">
+                        <FaHospitalAlt className="text-6xl" />
                         <h3>Rua Manoel Eudóxio Pereira, 826 - Centro. CEP 68900-021. Macapá / Amapá.</h3>
                     </div>
 
-                    <h2 className="font-bold text-xl">Entre em contato</h2>
-                    
-                    <div className="flex items-center gap-2">
-                        <FaHeadset className="text-6xl"/>
-                        <h3>(96)98116-3765</h3>
+                    <h2 className="font-bold text-xl">Contato</h2>
+
+                    <div className="flex justify-center items-center gap-5">
+                        <FaMobileAlt className="text-5xl" />
+                        <a href="https://wa.me/5596981163765" target="_blank" rel="noreferrer">
+                            <h3 className="text-xl">(96)98116-3765</h3>
+                        </a>
                     </div>
-                </div>    
+                </div>
             </div>
 
         </div>
