9dce9b92da6e6512bf148efe2fd063da8f493a0f
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-25 11:52:52 -0300

    ajustes de margem card depoimentos

--- src/components/CardDepositions.js
+++ src/components/CardDepositions.js
@@ -3,8 +3,8 @@ import { FaGoogle, FaQuoteLeft, FaQuoteRight, FaStar } from "react-icons/fa";
 
 function CardDepositions({ img, depText, depName }) {
   return (
-    <div className="flex flex-wrap justify-center shadow-lg mx-auto max-w-3xl md:mt-5 group transform duration-500 hover:-translate-y-2 rounded-2xl overflow-hidden cursor-pointer">
-      <div className="flex flex-col justify-center">
+    <div className="flex flex-wrap justify-center shadow-lg mx-auto max-w-3xl md:mt-5 py-5 group transform duration-500 hover:-translate-y-2 dark:bg-gray-800 rounded-2xl overflow-hidden cursor-pointer">
+      <div className="flex flex-col justify-center items-center">
         <img
           className="w-3/5 lg:w-36 mb-3 object-cover"
           src={img}
