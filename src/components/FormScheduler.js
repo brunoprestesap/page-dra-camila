53938317819a0c159ad83e517330e947cc6e75bb
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-25 13:01:28 -0300

    ajustes de margem card depoimentos

--- src/components/FormScheduler.js
+++ src/components/FormScheduler.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 
 import { FaWhatsapp } from "react-icons/fa";
 
 function FormScheduler({ color }) {
   return (
-    <div id="scheduler" className="flex items-center flex-col p-8">
-      <h2 className="text-4xl font-bold text-center uppercase">
+    <div id="scheduler" className="flex items-center flex-col p-5">
+      <h2 className="text-2xl md:text-4xl mb-5 font-bold text-center uppercase">
         AGENDE SUA CONSULTA
       </h2>
 
-      <div className="flex flex-col items-center w-full bg-white md:p-12 p-5 lg:max-w-4xl shadow-2xl dark:bg-gray-800 dark:text-gray-200">
-        <h2 className="text-xl text-center md:text-left leading-7">
-          Não deixe seus problemas de saúde mental afetarem sua vida, agende uma
-          consulta agora e juntos encontraremos um plano de
-          tratamento para aliviar seus sintomas e melhorar seu bem-estar.
+      <div className="flex flex-col items-center rounded-2xl bg-cover duration-500 w-full md:p-12 p-5 lg:max-w-4xl shadow-2xl dark:bg-gray-800 dark:text-gray-200">
+        <h2 className="text-base leading-7 md:text-xl text-justify md:text-left md:leading-7">
+          O acompanhamento psiquiátrico é uma das ferramentas do tratamento do sofrimento mental.
+          Assim como o corpo, nossa mente também precisa de atenção. 
+          Agende sua consulta e juntos encontraremos o melhor plano de cuidados para você.
         </h2>
         <button
-          className="p-5 w-80 m-5 text-white rounded-full transform duration-500 hover:-translate-y-1"
+          className="p-3 md:p-5 w-80 my-5 text-white rounded-2xl transform duration-500 hover:-translate-y-1"
           style={{ background: color }}
         >
-          <a href="https://wa.me/5596981163765" target="_blank" rel="noreferrer">
+          <a
+            href="https://wa.me/5596981163765"
+            target="_blank"
+            rel="noreferrer"
+          >
             <div className="flex content-center justify-around">
-              <h2 className="text-2xl ml-5 mr-5">
-                <FaWhatsapp />
-              </h2>
-              <h2 className="text-2xl">Agende sua consulta</h2>
+              <FaWhatsapp size={30} />
+              <h2 className="text-xl md:text-2xl">Agende sua consulta</h2>
             </div>
           </a>
         </button>
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,25 +4,23 @@ import { FaWhatsapp } from "react-icons/fa";
 function Presentation({ color, showForm }) {
   return (
     <div className="content bg-white md:p-12 p-4 pt-8 lg:max-w-3xl w-full lg:absolute top-40 right-5 shadow-2xl dark:bg-gray-800 dark:text-gray-200">
-      <h1 className="text-4xl font-bold">
+      <h1 className="text-2xl md:text-4xl font-bold">
         Encontre ajuda para seus problemas emocionais com a Dra. Camila Neiva
       </h1>
 
-      <h2 className="text-base mt-5 leading-7 pr-16">
+      <h2 className="text-lg text-justify mt-5 leading-7 md:pr-16">
       Estou aqui para ouvir suas preocupações e junto com você buscar maneiras de melhorar sua saúde mental e reencontrar sua paz e bem-estar.
       Vamos juntos definir o melhor plano de tratamento para você, seja ele com terapia, mudanças no estilo de vida, medicamentos ou uma combinação deles.
       </h2>
 
-      <div className="text-center text-[20px] pt-10 pb-10">
+      <div className="flex justify-center text-center text-[20px] pt-10 pb-10">
         <button
           className="p-5 w-80 text-white rounded-full transform duration-500 hover:-translate-y-1"
           style={{ background: color }}
         >
           <a href="https://wa.me/5596981163765" target="_blank" rel="noreferrer">
             <div className="flex content-center justify-around">
-              <span className="text-2xl">
-                <FaWhatsapp />
-              </span>
+              <FaWhatsapp size={30}/>
               <h2 className="text-2xl">Agende sua consulta</h2>
             </div>
           </a>
