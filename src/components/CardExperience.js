2bf4b3b8abeebcd899d81afe9652926625d27c98
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-26 19:15:44 -0300

    alteração das cores

--- src/components/CardExperience.js
+++ src/components/CardExperience.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const CardExperience = () => {
   return (
-    <div className="w-full mx-auto group shadow-2xl rounded-bl-2xl transform duration-500 hover:-translate-y-2 bg-orange-100 text-gray-700 p-4 lg:py-8 lg:px-5">
+    <div className="w-full mx-auto group shadow-2xl rounded-bl-2xl transform duration-500 hover:-translate-y-2 bg-orange-100 text-orange-900 p-4 lg:py-8 lg:px-5">
         <h2 className='text-xl lg:text-3xl'>Experiência</h2>
 
-        <div className='flex justify-between items-center mt-8 border-l-4 pl-2 border-l-gray-700'>
+        <div className='flex justify-between items-center mt-8 border-l-4 pl-2 border-l-orange-600'>
             <h2 className='text-2xl lg:text-5xl'>4+</h2>
             <h2 className='text-sm lg:text-xl text-right'>Anos de<br/>experëncia</h2>
         </div>
 
-        <div className='flex justify-between items-center mt-4 border-l-4 pl-2 border-l-gray-700'>
+        <div className='flex justify-between items-center mt-4 border-l-4 pl-2 border-l-orange-600'>
             <h2 className='text-2xl lg:text-5xl'>1K+</h2>
             <h2 className='text-sm lg:text-xl text-right'>Pacientes<br/>atendidos</h2>
         </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ function Hero({ color }) {
 
       <div className="w-full h-screen absolute top-0 left-0 md:bottom-4 bg-white/50">
 
-        <div className="invisible md:visible absolute top-0 left-0 md:w-64 lg:w-80 p-5 rounded-br-xl bg-gray-700">
+        <div className="invisible md:visible absolute top-0 left-0 md:w-64 lg:w-80 p-5 rounded-br-xl bg-orange-900">
           <img src={imgLogo} alt="logomarca"/>
         </div>
 
@@ -23,10 +23,10 @@ function Hero({ color }) {
         </div>
 
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-end px-5 py-12">
-          <h1 className="text-3xl text-center mb-5 md:text-4xl font-bold text-gray-800">
+          <h1 className="text-3xl text-center mb-5 md:text-4xl font-bold text-orange-900">
             Dra. Camila Neiva
           </h1>
-          <h2 className="text-sm text-justify md:text-lg text-gray-700 rounded-2xl bg-orange-100 p-3 leading-6">
+          <h2 className="text-sm text-justify md:text-lg text-orange-900 rounded-2xl bg-orange-100 p-3 leading-6">
           Tenho 38 anos, sou goiana de coração, médica formada pela Universidade Federal do Amapá.
           Cursei pós-graduação em Psiquiatria pelo Albert Einstein do Rio de Janeiro e Psiquiatria Intervencionista pelo Hospital de Clínicas da USP. Além disso, fiz Enfermagem e Mestrado pela UNICAMP.
           Trabalho desde 2019 ajudando pessoas a encontrar equilíbrio e tranquilidade emocional. 
