684808951c6aa716ceef0ae6104ce940031d1a23
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-25 22:57:13 -0300

    ajuste no topo

--- src/components/CardExperience.js
+++ src/components/CardExperience.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CardExperience = () => {
   return (
     <div className="w-full mx-auto group shadow-2xl rounded-b-2xl transform duration-500 hover:-translate-y-2 bg-white dark:bg-gray-800 p-4 lg:py-8 lg:px-5">
-        <h2 className='text-xl'>Experiência</h2>
+        <h2 className='text-xl lg:text-3xl'>Experiência</h2>
 
         <div className='flex justify-between items-center mt-8 border-l-4 pl-2'>
             <h2 className='text-2xl lg:text-5xl'>4+</h2>
