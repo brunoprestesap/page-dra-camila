a8ebcc43aff3ec0070d1c8944d6b450cf8816df9
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-27 14:15:21 -0300

    Carousel videos completo

--- src/components/ContentsVideo.js
+++ src/components/ContentsVideo.js
@@ -1,57 +1,99 @@
-import React from 'react'
+import React, { useState } from "react";
 
 import VideoPlayer from "./VideoPlayer.js";
-import depressao from '../assets/depressao.mp4'
-import babyblues from '../assets/babyblues.mp4'
-import depressaoCoisaSeria from '../assets/depressao-coisa-seria.mp4'
-import mitos from '../assets/mitos.mp4'
-import depressaoCapa from '../assets/capa/depressao.jpg'
-import babybluesCapa from '../assets/capa/baby-blues.jpg'
-import depressaoCoisaSeriaCapa from '../assets/capa/depresao-coisa-seria.jpg'
-import mitosCapa from '../assets/capa/depressao.jpg'
+import depressao from "../assets/depressao.mp4";
+import babyblues from "../assets/babyblues.mp4";
+import depressaoCoisaSeria from "../assets/depressao-coisa-seria.mp4";
+import mitos from "../assets/mitos.mp4";
+import depressaoCapa from "../assets/capa/depressao.jpg";
+import babybluesCapa from "../assets/capa/baby-blues.jpg";
+import depressaoCoisaSeriaCapa from "../assets/capa/depresao-coisa-seria.jpg";
+import mitosCapa from "../assets/capa/depressao.jpg";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 function Contents({ color }) {
   const contents = [
     {
-      id: '1',
-      title: 'Depressão',
+      id: "1",
+      title: "Depressão",
       videoId: depressao,
-      poster: depressaoCapa
+      poster: depressaoCapa,
     },
     {
-        id: '2',
-        title: 'Baby Blues',
-        videoId: babyblues,
-        poster: babybluesCapa
-      },
-      {
-        id: '3',
-        title: 'Depressão é coisa séria',
-        videoId: depressaoCoisaSeria,
-        poster: depressaoCoisaSeriaCapa
-      },
-      {
-        id: '4',
-        title: 'Mitos',
-        videoId: mitos,
-        poster: mitosCapa
-      },
+      id: "2",
+      title: "Baby Blues",
+      videoId: babyblues,
+      poster: babybluesCapa,
+    },
+    {
+      id: "3",
+      title: "Depressão é coisa séria",
+      videoId: depressaoCoisaSeria,
+      poster: depressaoCoisaSeriaCapa,
+    },
+    {
+      id: "4",
+      title: "Mitos",
+      videoId: mitos,
+      poster: mitosCapa,
+    },
   ];
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function prevSlide() {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? contents.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+
+  function nextSlide() {
+    const isLastSlide = currentIndex === contents.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
+  function goToSlide(slideIndex) {
+    setCurrentIndex(slideIndex);
+  }
+
   return (
-    <div id="conteudo" className="pl-5 pr-5">
-      <h2 className="text-[26px] font-bold text-center mt-8">Meus conteúdos</h2>
-      <div className="grid lg:flex lg:justify-center grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-5 md:mt-5">
-        {contents.map((content, i) => {
-          return (
-            <VideoPlayer
-              key={content.id}
-              title={content.title}
-              videoId={content.videoId}
-              poster={content.poster}
-            />
-          );
-        })}
+    <div id="depoimentos">
+      <div className="my-5">
+        <h2 className="text-xl md:text-3xl font-bold text-center">
+          O que as pessoas falam sobre o nosso atendimento
+        </h2>
+      </div>
+
+      <div className="w-full md:w-2/5 m-auto py-4 px-2 relative flex justify-center">
+        <div className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
+          <VideoPlayer
+            key={contents[currentIndex].poster}
+            title={contents[currentIndex].title}
+            videoId={contents[currentIndex].videoId}
+            poster={contents[currentIndex].poster}
+          />
+        </div>
+        {/* Left Arrow */}
+        <div className="absolute top-[40%] -translate-x-0 translate-y-[50%] left-5 md:left-20 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+        {/* Right Arrow */}
+        <div className="absolute top-[40%] -translate-x-0 translate-y-[50%] right-5 md:right-20 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {contents.map((slide, slideIndex) => (
+          <div
+            key={slide.id}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,10 +5,10 @@ function VideoPlayer({ id, title, videoId, poster }) {
   return (
     <div
       key={id}
-      className="mx-auto group shadow-2xl rounded-b-2xl transform duration-500 hover:-translate-y-2"  
+      className="mx-5 group shadow-2xl rounded-b-2xl transform duration-500 hover:-translate-y-2"  
     >
-      <h2 className='text-xl font-medium md:mb-2 text-center'>{title}</h2>
-      <video className='w-full h-[600px] rounded-2xl' controls poster={poster}>
+      <h2 className='text-xl font-bold m-5 text-center text-stone-700'>{title}</h2>
+      <video className='w-full h-[550px] rounded-2xl' controls poster={poster}>
         <source src={videoId} type="video/mp4"/>
       </video>
     </div>
