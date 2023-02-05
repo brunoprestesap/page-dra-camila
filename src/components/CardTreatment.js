0ae22365eb7af62ca3b1f523e355f614cb213db0
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-04 21:55:00 -0300

    ajustes na secao apresentacao

--- src/components/CardTreatment.js
+++ src/components/CardTreatment.js
@@ -9,7 +9,7 @@ function CardTreatment({ id, icon, title, text }) {
       <div className="p-5">
         <div className="mb-3 flex justify-center text-6xl">{icon}</div>
         <div>
-          <h2 className="text-2xl text-center font-bold my-5">{title}</h2>
+          <h2 className="text-2xl text-center font-bold uppercase my-5">{title}</h2>
           <h2 className="text-lg text-justify leading-7 text-white">{text}</h2>
         </div>
       </div>
