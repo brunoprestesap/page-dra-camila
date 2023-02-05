9843d874d40e0501d6e1a9016e632d8fe87502ef
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-04 23:53:54 -0300

    ajuste secao de diferenciais

--- src/components/CardBeneficios.js
+++ src/components/CardBeneficios.js
@@ -4,15 +4,15 @@ function CardBeneficios({ id, icon, title, text }) {
   return (
     <div
       key={id}
-      className="mx-auto group shadow-2xl rounded-b-2xl transform duration-500 hover:-translate-y-2 bg-[#003E53]/70 dark:bg-gray-800"
+      className="mx-auto group shadow-2xl rounded-b-2xl transform duration-500 hover:-translate-y-2"
     >
       <div className="p-5">
         <div className="flex justify-center">
           {icon}
         </div>
         <div className='w-full py-5'>
-          <h5 className="text-2xl font-bold text-center tracking-tight text-white">{title}</h5>
-          <p className='mt-3 leading-7 text-white'>{text}</p>
+          <h5 className="text-2xl font-bold text-center tracking-tight">{title}</h5>
+          <p className='mt-3 leading-7 text-stone-800'>{text}</p>
         </div>
       </div>
     </div>
