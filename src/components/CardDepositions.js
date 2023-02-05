1abaa34f41a560b352a511d887f5a4e7eb5ddf9a
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-04 22:04:17 -0300

    ajustes na secao apresentacao

--- src/components/CardDepositions.js
+++ src/components/CardDepositions.js
@@ -4,12 +4,12 @@ import { FaGoogle, FaQuoteLeft, FaQuoteRight, FaStar } from "react-icons/fa";
 
 function CardDepositions({ depText, depName }) {
   return (
-    <div className="flex flex-wrap justify-center shadow-lg mx-auto max-w-3xl md:mt-5 py-5 group transform duration-500 hover:-translate-y-2 bg-[#003E53]/70 dark:bg-[#003E53]/80 rounded-2xl overflow-hidden cursor-pointer">
+    <div className="flex flex-wrap justify-center shadow-lg mx-auto max-w-3xl md:mt-5 py-5 group transform duration-500 cursor-pointer">
       <div className="flex flex-col justify-center items-center">
         <div className="rounded-full border-2 p-5 bg-[#003E53]/30 border-[#003E53] my-3">
           <RiUserStarFill size={50}/>
         </div>
-        <div className="flex justify-center gap-2">
+        <div className="flex justify-center gap-2 mb-5">
           <FaStar color="FF884B" />
           <FaStar color="FF884B" />
           <FaStar color="FF884B" />
@@ -18,14 +18,14 @@ function CardDepositions({ depText, depName }) {
         </div>
       </div>
 
-      <div className="leading-normal">
-        <div className="p-4">
+      <div className="w-full">
+        <div className="px-5">
           <div className="md:flex md:items-center text-base md:text-xl font-normal md:gap-5">
             <FaQuoteLeft className="invisible md:visible" size={25} />
-            <h2 className="text-white">{depText}</h2>
+            <h2 className="leading-6">{depText}</h2>
             <FaQuoteRight className="invisible md:visible" size={25} />
           </div>
-          <div className="flex flex-row mt-5 items-center font-medium tracking-tight">
+          <div className="flex flex-row items-center font-medium">
             <FaGoogle color="#003E53" size={25} />
             <div className="text-sm md:text-xl font-bold ml-5">{depName} - Google Locais</div>
           </div>
