b2f383801f30489412e1e170bfb45dfae431131c
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-05 07:25:08 -0300

    ajuste no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import logoImg from "../assets/logo.png";
 
 import {
   FaFacebookF,
+  FaHeadset,
+  FaHospital,
   FaInstagram,
-  FaWhatsapp,
+  FaMailBulk,
 } from "react-icons/fa";
 
 const Footer = () => {
@@ -15,7 +17,9 @@ const Footer = () => {
           <img src={logoImg} className="w-5/6 mb-5" alt="logo" />
 
           <div>
-            <h2 className="text-xl font-bold text-[#CDBCA8]">Redes Sociais</h2>
+            <h2 className="text-xl font-bold text-[#CDBCA8] uppercase">
+              Redes Sociais
+            </h2>
             <div className="flex gap-3 my-3">
               <div>
                 <a
@@ -42,19 +46,22 @@ const Footer = () => {
             </div>
           </div>
 
-
           <div className="w-full my-5">
-            <h2 className="text-base font-bold text-[#CDBCA8]">PSIQUIATRA MACAPÁ - DRA CAMILA NEIVA</h2>
-            <div>
-              <a
-                href="https://wa.me/5596981163765"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h2 className="text-xl text-stone-200">55(96)981163765</h2>
-              </a>
+            <h2 className="text-base font-bold text-[#CDBCA8] my-5">
+              PSIQUIATRA MACAPÁ - DRA CAMILA NEIVA
+            </h2>
+            <div className="flex gap-3 items-center mb-3">
+              <FaHeadset size={30} color="CDBCA8" />
+              <h2 className="text-xl text-stone-200">55(96)981163765</h2>{" "}
             </div>
-            <div>
+            <div className="flex gap-3 items-center mb-3">
+              <FaHospital size={30} color="CDBCA8" />
+              <h2 className="text-base text-stone-200">
+                Rua Manoel Eudóxio Pereira, 826 <br /> Centro / Macapá-Amapá
+              </h2>
+            </div>
+            <div className="flex gap-3 items-center mb-3">
+              <FaMailBulk size={30} color="CDBCA8" />
               <h2 className="text-base text-stone-200">
                 contato@dracamilaneiva.com.br
               </h2>
@@ -62,7 +69,9 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h2 className="text-xl font-bold text-[#CDBCA8]">Navegação</h2>
+          <h2 className="text-xl font-bold text-[#CDBCA8] uppercase">
+            Navegação
+          </h2>
           <a href="#hero">
             <h2 className="text-base my-2 mx-3 text-stone-200">Home</h2>
           </a>
@@ -84,29 +93,6 @@ const Footer = () => {
           <a href="#contact">
             <h2 className="text-base my-2 mx-3 text-stone-200">Localização</h2>
           </a>
-        </div>
-
-        <div className="flex flex-col">
-          <h2 className="text-2xl font-bold text-[#CDBCA8 ]">
-            Reencontre sua paz
-            <br /> e bem-estar
-          </h2>
-          <h2 className="text-xl text-stone-200 my-5">
-            Agende sua consulta
-            <br /> agora conosco
-          </h2>
-          <button className="p-3 w-72 text-white rounded-2xl transform duration-500 hover:-translate-y-1 bg-[#003E53]/70 border-2 border-white hover:bg-[#003E53]/90 hover:border-2 hover:border-stone-800">
-            <a
-              href="https://wa.me/5596981163765"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex justify-center items-center gap-3 md:gap-5">
-                <FaWhatsapp size={30} />
-                <h2 className="text-sm md:text-xl">Agende sua consulta</h2>
-              </div>
-            </a>
-          </button>
         </div>
       </div>
     </div>
