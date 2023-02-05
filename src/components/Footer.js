e53c79ad07bdcf1d1345e263463c69edeb56005d
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-05 13:24:54 -0300

    Ajustes no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,9 @@ const Footer = () => {
     <div id="footer" className="bg-[#003E53] p-5 md:p-12">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
         <div>
-          <img src={logoImg} className="w-5/6 mb-5" alt="logo" />
+          <div className="flex justify-center md:justify-start">
+            <img src={logoImg} className="w-5/6 mb-5" alt="logo" />
+          </div>
 
           <div>
             <h2 className="text-xl font-bold text-[#CDBCA8] uppercase">
@@ -47,23 +49,25 @@ const Footer = () => {
           </div>
 
           <div className="w-full my-5">
-            <h2 className="text-base font-bold text-[#CDBCA8] my-5">
-              PSIQUIATRA MACAPÁ - DRA CAMILA NEIVA
-            </h2>
+            <div className="flex my-5">
+              <h2 className="text-base font-bold text-[#CDBCA8]">
+                PSIQUIATRA MACAPÁ - DRA CAMILA NEIVA
+              </h2>
+            </div>
             <div className="flex gap-3 items-center mb-3">
               <FaHeadset size={30} color="CDBCA8" />
-              <h2 className="text-xl text-stone-200">55(96)981163765</h2>{" "}
+              <h2 className="font-mono text-xl text-white subpixel-antialiased">55(96)981163765</h2>{" "}
             </div>
             <div className="flex gap-3 items-center mb-3">
               <FaHospital size={30} color="CDBCA8" />
-              <h2 className="text-base text-stone-200">
+              <h2 className="text-base text-white">
                 Rua Manoel Eudóxio Pereira, 826 <br /> Centro / Macapá-Amapá
               </h2>
             </div>
             <div className="flex gap-3 items-center mb-3">
               <FaMailBulk size={30} color="CDBCA8" />
-              <h2 className="text-base text-stone-200">
-                contato@dracamilaneiva.com.br
+              <h2 className="text-base text-white">
+                clinicadracamilaneiva@gmail.com
               </h2>
             </div>
           </div>
@@ -73,25 +77,25 @@ const Footer = () => {
             Navegação
           </h2>
           <a href="#hero">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Home</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Home</h2>
           </a>
           <a href="#sobre">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Sobre</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Sobre</h2>
           </a>
           <a href="#tratamentos">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Tratamentos</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Tratamentos</h2>
           </a>
           <a href="#depoimentos">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Depoimentos</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Depoimentos</h2>
           </a>
           <a href="#diferenciais">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Diferenciais</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Diferenciais</h2>
           </a>
           <a href="#conteudos">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Conteúdos</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Conteúdos</h2>
           </a>
           <a href="#contact">
-            <h2 className="text-base my-2 mx-3 text-stone-200">Localização</h2>
+            <h2 className="text-base my-2 mx-3 text-white">Localização</h2>
           </a>
         </div>
       </div>
