eb59f58e2db4232bbd95b32009b82c517782592f
Bruno Prestes <brunoasprestes@gmail.com>
2023-02-13 21:38:29 -0300

    Tratamentos iniciado

--- src/components/Cannabis/CardTratamento.js
+++ src/components/Cannabis/CardTratamento.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const CardTratamento = ({ id, icon, title, text }) => {
   return (
-    <div
-      key={id}
-      className="group"
-    >
+    <div key={id} className="group px-5">
       <div>
+
         <div className="mb-3 flex justify-center items-center gap-3">
           {icon}
-          <h2 className="text-2xl text-center font-bold text-white uppercase my-5">{title}</h2>
+          <h2 className="text-2xl text-center font-bold text-white uppercase">
+            {title}
+          </h2>
         </div>
+
         <div>
-          <h2 className="text-lg text-justify leading-7 text-slate-900 xl:px-16">{text}</h2>
+          <h2 className="text-base text-justify text-slate-900">{text}</h2>
         </div>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardTratamento
+export default CardTratamento;
